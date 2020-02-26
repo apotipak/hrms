e7cd52331add51ae8b70cafc085923bb6a57bf75
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2020-02-26 21:29:17 +0700

    Add initial files.

--- system/forms.py
+++ system/forms.py
@@ -6,12 +6,11 @@ class CompanyApprovePriorityForm(forms.ModelForm):
         model = TAprove
         fields = ('apr_id', 'apr_name_th', 'apr_pos_th', 'apr_piority', )
 
-    """
+
     def __init__(self, *args, **kwargs):
     	super(CompanyApprovePriorityForm, self).__init__(*args, **kwargs)
     	self.fields['apr_id'].label = "Approve ID"
     	self.fields['apr_name_th'].label = "Name (TH)"
     	self.fields['apr_pos_th'].label = "Position (TH)"
     	self.fields['apr_piority'].label = "Priority Type"    	
-	"""
-	
+
