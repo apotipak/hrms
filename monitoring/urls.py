1e53522e1be2e7ed0bb2ea0292d0a4bb9c7bf3c3
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2020-10-22 14:06:00 +0700

    Add initial files.

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.MonitoringList, name='monitoring-list'),    
+    # path('', views.MonitoringList, name='monitoring-list'),
+    path('', views.ScheduleMaintenance, name='schedule-maintenance'),
 ]
--- monitoring/views.py
+++ monitoring/views.py
@@ -4,6 +4,19 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
 
+
+@login_required(login_url='/accounts/login/')
+@permission_required('monitoring.view_monitoring', login_url='/accounts/login/')
+def ScheduleMaintenance(request):
+	page_title = settings.PROJECT_NAME
+	db_server = settings.DATABASES['default']['HOST']
+	project_name = settings.PROJECT_NAME
+	project_version = settings.PROJECT_VERSION
+	today_date = settings.TODAY_DATE	
+
+	return render(request, 'monitoring/schedule_maintenance.html', {'page_title': page_title, 'project_name': project_name, 'project_version': project_version, 'db_server': db_server, 'today_date': today_date})
+
+'''
 @login_required(login_url='/accounts/login/')
 @permission_required('monitoring.view_monitoring', login_url='/accounts/login/')
 def MonitoringList(request):
@@ -14,3 +27,6 @@ def MonitoringList(request):
 	today_date = settings.TODAY_DATE	
 
 	return render(request, 'monitoring/monitoring_list.html', {'page_title': page_title, 'project_name': project_name, 'project_version': project_version, 'db_server': db_server, 'today_date': today_date})	
+'''
+
+
