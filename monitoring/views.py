5171f476e38eea447efd683804fa7926c1f2205d
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2020-10-26 10:53:10 +0700

    Add initial files.

--- monitoring/views.py
+++ monitoring/views.py
@@ -49,127 +49,121 @@ def ajax_get_customer(request):
 	if cnt_id is not None:
 		try:
 			cus_contract = CusContract.objects.filter(cnt_id=cnt_id).get()
-			print("found")
+			cnt_doc_no = cus_contract.cnt_doc_no
 
 			try:
 			    customer = Customer.objects.filter(cus_id=cus_id, cus_brn=cus_brn).get()
 			    
-			    cus_name_th = customer.cus_name_th
-			    cus_sht_th = customer.cus_sht_th
-			    cus_add1_th = customer.cus_add1_th
-			    cus_add2_th = customer.cus_add2_th
-			    cus_subdist_th = customer.cus_subdist_th
-
-			    cus_name_en = customer.cus_name_en
-			    cus_sht_en = customer.cus_sht_en
-			    cus_add1_en = customer.cus_add1_en
-			    cus_add2_en = customer.cus_add2_en
-			    cus_subdist_en = customer.cus_subdist_en
-
-			    cus_zip = customer.cus_zip
+			    cus_site_name_th = customer.cus_name_th
+			    cus_site_sht_th = customer.cus_sht_th
+			    cus_site_add1_th = customer.cus_add1_th
+			    cus_site_add2_th = customer.cus_add2_th
+			    cus_site_subdist_th = customer.cus_subdist_th
+			    cus_site_name_en = customer.cus_name_en
+			    cus_site_sht_en = customer.cus_sht_en
+			    cus_site_add1_en = customer.cus_add1_en
+			    cus_site_add2_en = customer.cus_add2_en
+			    cus_site_subdist_en = customer.cus_subdist_en
+			    cus_site_zip = customer.cus_zip
 
 			    if customer.cus_district_id is not None:
-			        cus_district_id = customer.cus_district_id
-			        cus_district_th = customer.cus_district.dist_th
-			        cus_district_en = customer.cus_district.dist_en
+			        cus_site_district_id = customer.cus_district_id
+			        cus_site_district_th = customer.cus_district.dist_th
+			        cus_site_district_en = customer.cus_district.dist_en
 			    else:
-			        cus_district_id = 0
-			        cus_district_th = ""
-			        cus_district_en = ""
+			        cus_site_district_id = 0
+			        cus_site_district_th = ""
+			        cus_site_district_en = ""
 
 			    if customer.cus_city_id is not None:
-			        cus_city_th = customer.cus_city.city_th
-			        cus_city_en = customer.cus_city.city_en
+			        cus_site_city_th = customer.cus_city.city_th
+			        cus_site_city_en = customer.cus_city.city_en
 			    else:
-			        cus_city_id = 0
-			        cus_city_th = ""
-			        cus_city_en = ""
+			        cus_site_city_id = 0
+			        cus_site_city_th = ""
+			        cus_site_city_en = ""
 
 			    if customer.cus_country_id is not None:
-			        cus_country_th = customer.cus_country.country_th
-			        cus_country_en = customer.cus_country.country_en
+			        cus_site_country_th = customer.cus_country.country_th
+			        cus_site_country_en = customer.cus_country.country_en
 			    else:
-			        cus_country_id = 0
-			        cus_country_th = ""
-			        cus_country_en = ""
+			        cus_site_country_id = 0
+			        cus_site_country_th = ""
+			        cus_site_country_en = ""
 			    
-			    cus_tel = customer.cus_tel
-			    cus_fax = customer.cus_fax
-			    cus_email = customer.cus_email
+			    cus_site_tel = customer.cus_tel
+			    cus_site_fax = customer.cus_fax
+			    cus_site_email = customer.cus_email
 			    
 			    if customer.site_contact_id is not None:
-			        site_contact_id_th = customer.site_contact_id
-			        site_contact_con_fname_th = customer.site_contact.con_fname_th
-			        site_contact_con_lname_th = customer.site_contact.con_lname_th
-			        site_contact_con_position_th = customer.site_contact.con_position_th
+			        cus_site_contact_id_th = customer.site_contact_id
+			        cus_site_contact_con_fname_th = customer.site_contact.con_fname_th
+			        cus_site_contact_con_lname_th = customer.site_contact.con_lname_th
+			        cus_site_contact_con_position_th = customer.site_contact.con_position_th
 
-			        site_contact_id_en = customer.site_contact_id
-			        site_contact_con_fname_en = customer.site_contact.con_fname_en
-			        site_contact_con_lname_en = customer.site_contact.con_lname_en
-			        site_contact_con_position_en = customer.site_contact.con_position_en
+			        cus_site_contact_id_en = customer.site_contact_id
+			        cus_site_contact_con_fname_en = customer.site_contact.con_fname_en
+			        cus_site_contact_con_lname_en = customer.site_contact.con_lname_en
+			        cus_site_contact_con_position_en = customer.site_contact.con_position_en
 			    else:
-			        site_contact_id_th = 0
-			        site_contact_con_fname_th = ""
-			        site_contact_con_lname_th = ""
-			        site_contact_con_position_th = ""
+			        cus_site_contact_id_th = 0
+			        cus_site_contact_con_fname_th = ""
+			        cus_site_contact_con_lname_th = ""
+			        cus_site_contact_con_position_th = ""
 
-			        site_contact_id_en = 0
-			        site_contact_con_fname_en = ""
-			        site_contact_con_lname_en = ""
-			        site_contact_con_position_en = ""
+			        cus_site_contact_id_en = 0
+			        cus_site_contact_con_fname_en = ""
+			        cus_site_contact_con_lname_en = ""
+			        cus_site_contact_con_position_en = ""
 
 
 			    if customer.cus_zone_id is not None:            
-			        site_contact_cus_zone_id = customer.cus_zone_id
-			        site_contact_cus_zone_th = customer.cus_zone.zone_th
-			        site_contact_cus_zone_en = customer.cus_zone.zone_en
+			        cus_site_contact_cus_zone_id = customer.cus_zone_id
+			        cus_site_contact_cus_zone_th = customer.cus_zone.zone_th
+			        cus_site_contact_cus_zone_en = customer.cus_zone.zone_en
 			    else:
-			        site_contact_cus_zone_id = 0
-			        site_contact_cus_zone_th = ""
-			        site_contact_cus_zone_en = ""
+			        cus_site_contact_cus_zone_id = 0
+			        cus_site_contact_cus_zone_th = ""
+			        cus_site_contact_cus_zone_en = ""
 
 			    response = JsonResponse(data={
-			        # TH
+			        # CUS_SITE
 			        "success": True,
-			        "class": "bg-danger",
-			        "message": "",
+			        "class": "bg-success",
 			        "is_existed": True,
-			        "cus_name_th": cus_name_th,
-			        "cus_sht_th": cus_sht_th,
-			        "cus_add_th": cus_add1_th + " " + cus_add2_th,
-			        "cus_subdist_th": cus_subdist_th,
-			        "cus_zip_th": cus_zip,
-			        "cus_district_id_th": cus_district_id,
-			        "cus_district_th": cus_district_th,
-			        "cus_city_th": cus_city_th,
-			        "cus_country_th": cus_country_th,
-			        "site_contact_id_th": site_contact_id_th,
-			        "site_contact_con_th": str(site_contact_id_th) + " | " + str(site_contact_con_fname_th) + " " + str(site_contact_con_lname_th),
-			        "site_contact_con_position_th": site_contact_con_position_th,
-			        
-			        # EN
-			        "cus_name_en": cus_name_en,
-			        "cus_sht_en": cus_sht_en,
-			        "cus_add_en": cus_add1_en + " " + cus_add2_en,
-			        "cus_subdist_en": cus_subdist_en,
-			        "cus_zip_en": cus_zip,
-			        "cus_district_id_en": cus_district_id,
-			        "cus_district_en": cus_district_en,
-			        "cus_city_en": cus_city_en,
-			        "cus_country_en": cus_country_en,                                                            
-			        "site_contact_id_en": site_contact_id_en,
-			        "site_contact_con_en": str(site_contact_id_en) + " | " + str(site_contact_con_fname_en) + " " + str(site_contact_con_lname_en),
-			        "site_contact_con_position_en": site_contact_con_position_en,                
 
-			        "cus_tel": cus_tel,
-			        "cus_fax": cus_fax,
-			        "cus_email": cus_email,
-			        "cus_zone_id": site_contact_cus_zone_id,
-			        "cus_zone_th": site_contact_cus_zone_th,
-			        "cus_zone_en": site_contact_cus_zone_en,
+			        "cnt_doc_no": cnt_doc_no,
+
+			        "cus_site_name_th": cus_site_name_th,
+			        "cus_site_sht_th": cus_site_sht_th,
+			        "cus_site_add_th": cus_site_add1_th + " " + cus_site_add2_th,
+			        "cus_site_subdist_th": cus_site_subdist_th,
+			        "cus_site_district_id": cus_site_district_id,
+			        "cus_site_district_th": cus_site_district_th,
+			        "cus_site_city_th": cus_site_city_th,
+			        "cus_site_country_th": cus_site_country_th,
+			        "cus_site_contact_id_th": cus_site_contact_id_th,
+			        "cus_site_contact_name_th": str(cus_site_contact_con_fname_th) + " " + str(cus_site_contact_con_lname_th),
+			        "cus_site_contact_position_th": cus_site_contact_con_position_th,
+			        "cus_site_name_en": cus_site_name_en,
+			        "cus_site_sht_en": cus_site_sht_en,
+			        "cus_site_add_en": cus_site_add1_en + " " + cus_site_add2_en,
+			        "cus_site_subdist_en": cus_site_subdist_en,
+			        "cus_site_zip": cus_site_zip,
+			        "cus_site_district_en": cus_site_district_en,
+			        "cus_site_city_en": cus_site_city_en,
+			        "cus_site_country_en": cus_site_country_en,                                                            
+			        "cus_site_contact_id_en": cus_site_contact_id_en,
+			        "cus_site_contact_con_en": str(cus_site_contact_id_en) + " | " + str(cus_site_contact_con_fname_en) + " " + str(cus_site_contact_con_lname_en),
+			        "cus_site_contact_con_position_en": cus_site_contact_con_position_en,                
+			        "cus_site_tel": cus_site_tel,
+			        "cus_site_fax": cus_site_fax,
+			        "cus_site_email": cus_site_email,
+			        "cus_site_contact_cus_zone_id": cus_site_contact_cus_zone_id,
+			        "cus_site_contact_cus_zone_th": cus_site_contact_cus_zone_th,
+			        "cus_site_contact_cus_zone_en": cus_site_contact_cus_zone_en,
 			    })
 			    response.status_code = 200
-			    return response
 			except Customer.DoesNotExist:            
 			    response = JsonResponse(data={
 			        "success": False,
