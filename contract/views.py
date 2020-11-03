068a4955732dda8b636582819fd624e70fcb243a
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2020-11-03 08:17:44 +0700

    Add initial files.

--- contract/views.py
+++ contract/views.py
@@ -29,8 +29,8 @@ from django.http import HttpResponse, Http404
 from django.http import FileResponse
 from django.db import connection
 import docx
-from docx2pdf import convert
-# import comtypes.client
+# from docx2pdf import convert
+import comtypes.client
 
 
 def check_modified_field(table_name, primary_key, field_name, old_value, new_value, log_type, request):
@@ -2498,14 +2498,14 @@ def generate_contract(request, *args, **kwargs):
 
 
     # docx2pdf
+    '''
     docx_file_name = MEDIA_ROOT + "/contract/download/" + file_name + ".docx"
-    pdf_file_name = MEDIA_ROOT + "/contract/download/" + file_name + ".pdf"
-    
+    pdf_file_name = MEDIA_ROOT + "/contract/download/" + file_name + ".pdf"    
     print("pdf_file_name = " + str(pdf_file_name))
     convert(docx_file_name, pdf_file_name)
-
-    # comtypes
     '''
+
+    # comtypes    
     wdFormatPDF = 17
     in_file = MEDIA_ROOT + "/contract/download/" + file_name + ".docx"
     out_file = MEDIA_ROOT + "/contract/download/" + file_name + ".pdf"
@@ -2514,7 +2514,6 @@ def generate_contract(request, *args, **kwargs):
     doc.SaveAs(out_file, FileFormat=wdFormatPDF)
     doc.Close()
     word.Quit()
-    '''
 
     return render(request, 'contract/generate_contract.html', context)
 
