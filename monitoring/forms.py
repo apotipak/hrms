3ec0dff4d72e1f1f192bfb62a15b94059cbe6540
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2021-02-12 07:09:39 +0700

    Add initial files.

--- monitoring/forms.py
+++ monitoring/forms.py
@@ -12,11 +12,11 @@ class ScheduleMaintenanceForm(forms.Form):
 
     def __init__(self, *args, **kwargs):    	
         super(ScheduleMaintenanceForm, self).__init__(*args, **kwargs)
-        self.fields['cus_id'].widget.attrs = {'class': 'form-control form-control-md col-2', 'placeholder': _('Customer ID')}        
+        self.fields['cus_id'].widget.attrs = {'class': 'form-control form-control-md col-2', 'placeholder': _('')}
         self.fields['cus_id'].error_messages = {'required': _('Please enter data'), 'max_value': _('Incorrect code')}
-        self.fields['cus_brn'].widget.attrs = {'class': 'form-control form-control-md col-2', 'placeholder': _('Customer Branch')}        
+        self.fields['cus_brn'].widget.attrs = {'class': 'form-control form-control-md col-2', 'placeholder': _('')}
         self.fields['cus_brn'].error_messages = {'required': _('Please enter data'), 'max_value': _('Incorrect code')}
-        self.fields['cus_vol'].widget.attrs = {'class': 'form-control form-control-md col-2', 'placeholder': _('Customer Volume')}        
+        self.fields['cus_vol'].widget.attrs = {'class': 'form-control form-control-md col-2', 'placeholder': _('')}
         self.fields['cus_vol'].error_messages = {'required': _('Please enter data'), 'max_value': _('Incorrect code')}
 
         '''
