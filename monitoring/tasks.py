a92ba6edd6d6fc7b114215c4ea06ae630f55e6cd
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2021-02-22 09:43:22 +0700

    Add initial files.

--- monitoring/tasks.py
+++ monitoring/tasks.py
@@ -3,6 +3,7 @@ from celery_progress.backend import ProgressRecorder
 import time
 
 
+'''
 @shared_task(bind=True)
 def my_task(self, seconds):
     progress_recorder = ProgressRecorder(self)
@@ -12,3 +13,21 @@ def my_task(self, seconds):
         result += i
         progress_recorder.set_progress(i + 1, seconds)
     return result
+'''
+
+@shared_task
+def do_something():
+    bar = ProgressBar(
+        task_id=do_something.request.id,
+        total=10,
+        step='Drying kelp...'
+    )
+
+    # some_work()
+    bar.update(
+        progress='5',
+        step='Making sushi...'
+    )
+
+    # some_more_work()
+    bar.progress.finalize()
