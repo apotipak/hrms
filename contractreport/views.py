be66281c308b7a8d64b6a4d5efe8088e259d6731
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2021-03-09 16:04:27 +0700

    Add initial files.

--- contractreport/views.py
+++ contractreport/views.py
@@ -55,7 +55,7 @@ def AJAXReportSearchContract(request):
 
 	sql = "select cnt_id, cus_name_en, cus_name_th, "
 	sql += "cnt_sign_frm, cnt_sign_to, cnt_eff_frm, "
-	sql += "cnt_eff_to, cnt_zone, nosupD, supD, nosupN, supN, Sun, Mon, Tue, Wed, Thu, Fri, Sat, Pub "
+	sql += "cnt_eff_to, cnt_zone, nosupD, supD, nosupN, supN, Sun, Mon, Tue, Wed, Thu, Fri, Sat, Pub, dept_sht "
 	sql += "from V_Contract_Summary "
 	sql += "where cnt_id>=" + str(contract_number_from) + " " 
 	sql += "and cnt_id<=" + str(contract_number_to) + " "
@@ -84,30 +84,67 @@ def AJAXReportSearchContract(request):
 	finally:
 	    cursor.close()
 	
+	total_sun = 0
+	total_mon = 0
+	total_tue = 0
+	total_wed = 0
+	total_thu = 0
+	total_fri = 0
+	total_sat = 0	
+	total_pub = 0
+
+	total_supD = 0
+	total_supN = 0
+
+	total_nosupD = 0
+	total_nosupN = 0
+
+	grand_total_sup_DN = 0
+	grand_total_nosup_DN = 0
+	grand_grand_total = 0
+
 	for row in contract_obj:
 		if row[3] is not None:
-			cnt_sign_frm = row[3].strftime("%d/%b/%Y")
+			cnt_sign_frm = row[3].strftime("%d-%b-%Y")
 		else:
 			cnt_sign_frm = ""
 
 		if row[4] is not None:
-			cnt_sign_to = row[4].strftime("%d/%b/%Y")
+			cnt_sign_to = row[4].strftime("%d-%b-%Y")
 		else:
 			cnt_sign_to = ""
 
 		if row[5] is not None:
-			cnt_eff_frm = row[5].strftime("%d/%b/%Y")
+			cnt_eff_frm = row[5].strftime("%d-%b-%Y")
 		else:
 			cnt_eff_frm = ""
 
 		if row[6] is not None:
-			cnt_eff_to = row[6].strftime("%d/%b/%Y")
+			cnt_eff_to = row[6].strftime("%d-%b-%Y")
 		else:
 			cnt_eff_to = ""
 
 		total_sup_DN = row[9] + row[11]
 		total_nosup_DN = row[8] + row[10]
 		grand_total = total_sup_DN + total_nosup_DN
+
+		total_supD += row[9]
+		total_supN += row[11]
+		total_nosupD += row[8]
+		total_nosupN += row[10]
+
+		total_sun += row[12]
+		total_mon += row[13]
+		total_tue += row[14]
+		total_wed += row[15]
+		total_thu += row[16]
+		total_fri += row[17]
+		total_sat += row[18]
+		total_pub += row[19]
+
+		grand_total_sup_DN += total_sup_DN
+		grand_total_nosup_DN += total_nosup_DN
+		grand_grand_total += grand_total
 
 		record = {
 			"cnt_id": row[0],					
@@ -118,6 +155,7 @@ def AJAXReportSearchContract(request):
 			"cnt_eff_frm": cnt_eff_frm,
 			"cnt_eff_to": cnt_eff_to,
 			"cnt_zone": row[7],
+			"dept_sht": row[20],
 			"nosupD": row[8],
 			"supD": row[9],
 			"nosupN": row[10],
@@ -140,6 +178,23 @@ def AJAXReportSearchContract(request):
 	    "is_error": is_error,
 	    "error_message": error_message,
 	    "contract_list": list(contract_list),
+		"total_sun": total_sun,
+		"total_mon": total_mon,
+		"total_tue": total_tue,
+		"total_wed": total_wed,
+		"total_thu": total_thu,
+		"total_fri": total_fri,
+		"total_sat": total_sat,
+		"total_pub": total_pub,
+
+		"total_supD": total_supD,
+		"total_supN": total_supN,
+		"total_nosupD": total_nosupD,
+		"total_nosupN": total_nosupN,
+
+		"grand_total_sup_DN": grand_total_sup_DN,
+		"grand_total_nosup_DN": grand_total_nosup_DN,
+		"grand_grand_total": grand_grand_total,
 	})
 
 	response.status_code = 200
