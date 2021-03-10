82a3d1e30c454b0d178a133cc860c8addedfd0db
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2021-03-10 08:46:04 +0700

    Add initial files.

--- contractreport/urls.py
+++ contractreport/urls.py
@@ -1,12 +1,11 @@
-from django.urls import path
 from django.conf.urls import url
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from django.urls import re_path
 from . import views
 
 
 urlpatterns = [
     path('', views.ContractListReport, name='contract_list_report'),    
-    url(r'^ajax/report_search_contract/$', views.AJAXReportSearchContract, name='report_search_contract'),
+    url(r'^ajax/report_search_contract/$', views.AJAXReportSearchContract, name='report_search_contract'),    
     url(r'^generate-contract-list/(?P<contract_number_from>\w+)/(?P<contract_number_to>\w+)/(?P<contract_status>\w+)/(?P<contract_zone>\w+)/$', views.generate_contract_list, name='generate-contract-list'),
 ]
