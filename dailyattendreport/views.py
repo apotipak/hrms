25eec167f0941b6d14af5003fc0ef9fb1e514635
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2021-03-31 11:34:10 +0700

    Add initial files.

--- dailyattendreport/views.py
+++ dailyattendreport/views.py
@@ -1187,7 +1187,7 @@ def PSNSlipD1Report(request):
             prd_date_to = period_list[0][7]
             prd_date_paid = period_list[0][8]
     '''
-    
+
     return render(request, 'dailyattendreport/psn_slip_d1_report.html',
         {
         'page_title': page_title, 
@@ -1243,4 +1243,85 @@ def GPM422NoOfGuardOperationByEmplByZoneReport(request):
         'dept_zone_obj': dept_zone_obj,
         })
 
+
+
+@permission_required('dailyattendreport.can_access_psn_slip_d1_report', login_url='/accounts/login/')
+def AjaxValidatePSNSlipD1Period(request):
+    page_title = settings.PROJECT_NAME
+    db_server = settings.DATABASES['default']['HOST']
+    project_name = settings.PROJECT_NAME
+    project_version = settings.PROJECT_VERSION  
+    
+    today_date = settings.TODAY_DATE.strftime("%d/%m/%Y")
+    contract_number_from = request.POST.get('contract_number_from')
+    contract_number_to = request.POST.get('contract_number_to')
+    start_date = request.POST.get('start_date')
+    end_date = request.POST.get('end_date')
+
+    contract_number_from = 0 if contract_number_from is None else contract_number_from
+    contract_number_to = 9999999999 if contract_number_to is None else contract_number_to
+    start_date = today_date if start_date is None else datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
+    end_date = today_date if end_date is None else datetime.datetime.strptime(end_date, "%d/%m/%Y").date()
+
+    sql = "select emp_fname_th, emp_lname_th, shf_desc, dept_en, cnt_id, "
+    sql += "emp_id, dly_date, sch_shift, dept_id, sch_rank, "
+    sql += "absent, relieft_id, tel_man, tel_paid, ot, "
+    sql += "ot_hr_amt, cus_name_th, late, late_full "
+    sql += "FROM V_HDLYPLAN "
+    sql += "WHERE absent = 0 AND (sch_shift <> 99 OR sch_shift <> 999) "
+    sql += "and (cnt_id>=" + str(contract_number_from) + " and cnt_id<=" + str(contract_number_to) + ") "
+    sql += "and (dly_date>='" + str(start_date) + "' and dly_date<='" + str(end_date) + "') "
+    sql += "ORDER BY cnt_id ASC, dly_date ASC, shf_desc ASC, emp_id ASC"
+    print(sql)
+    
+    dly_plan_obj = None
+    record = {}
+    dly_plan_list = []
+    error_message = ""
+
+    try:                
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        dly_plan_obj = cursor.fetchall()        
+    except db.OperationalError as e:
+        error_message = "<b>Error: please send this error to IT team</b><br>" + str(e)
+    except db.Error as e:
+        error_message = "<b>Error: please send this error to IT team</b><br>" + str(e)
+    finally:
+        cursor.close()
+
+    if dly_plan_obj is not None:
+        if len(dly_plan_obj)>0:           
+            for item in dly_plan_obj:             
+                record = {
+                    "emp_fname_th": item[0],
+                    "emp_lname_th": item[1],
+                    "shf_desc": item[2],
+                    "dept_en": item[3],
+                    "cnt_id": item[4],
+                    "emp_id": item[5],
+                    "dly_date": item[6].strftime("%d/%m/%Y"),
+                    "sch_shift": item[7],
+                    "dept_id": item[8],
+                    "sch_rank": item[9],
+                    "absent": item[10],
+                    "relieft_id": item[11],
+                    "tel_man": item[12],
+                    "tel_paid": item[13],
+                    "ot": item[14],
+                    "ot_hr_amt": item[15],
+                    "cus_name_th": item[16],
+                    "late": item[17],
+                    "late_full": item[18],
+                }
+                dly_plan_list.append(record)
+
+    response = JsonResponse(data={        
+        "is_error": False,
+        "error_message": error_message,
+        "dly_plan_list": list(dly_plan_list),
+    })
+
+    response.status_code = 200
+    return response
 
